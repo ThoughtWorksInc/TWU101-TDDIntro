36ecf752a7d1562338f5d1fa945c9afbea0c48df
Bill Schofield <bill@billschofield.net>
2014-04-09 19:27:56 -0500

    Second test passes

--- src/main/java/com/thoughtworks/string_joiner/StringJoiner.java
+++ src/main/java/com/thoughtworks/string_joiner/StringJoiner.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 public class StringJoiner {
     public String join(List<String> strings) {
+        if (strings.size() > 0){
+            return strings.get(0);
+        }
         return "";
     }
 }
