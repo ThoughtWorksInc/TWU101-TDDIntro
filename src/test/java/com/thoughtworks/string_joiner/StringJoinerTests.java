9c6e4976a777a750f45f9b7e23a559906dfe3431
Bill Schofield <bill@billschofield.net>
2014-04-09 19:28:32 -0500

    Condensed tests and removed comments

--- src/test/java/com/thoughtworks/string_joiner/StringJoinerTests.java
+++ src/test/java/com/thoughtworks/string_joiner/StringJoinerTests.java
@@ -11,26 +11,18 @@ import static org.junit.Assert.assertThat;
 public class StringJoinerTests {
     @Test
     public void shouldJoinIntoAnEmptyStringWhenListIsEmpty(){
-        // Arrange
         List<String> strings = new ArrayList<String>();
-        final StringJoiner joiner = new StringJoiner();
-
-        // Action
+         StringJoiner joiner = new StringJoiner();
         String result = joiner.join(strings);
-
-        // Assert
         assertThat(result, is(""));
     }
-
     @Test
     public void shouldJoinIntoTheStringWhenListIsOneString(){
         List<String> strings = new ArrayList<String>();
         String aString = "A String";
         strings.add(aString);
-        final StringJoiner joiner = new StringJoiner();
-
+        StringJoiner joiner = new StringJoiner();
         String result = joiner.join(strings);
-
         assertThat(result, is(aString));
     }
 }
