bf0d4730e56576f524adc501dcee35a0dd2c19cb
Bill Schofield <bill@billschofield.net>
2014-04-10 16:25:15 -0500

    Third failing test

--- src/test/java/com/thoughtworks/string_joiner/StringJoinerTests.java
+++ src/test/java/com/thoughtworks/string_joiner/StringJoinerTests.java
@@ -6,25 +6,37 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.both;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class StringJoinerTests {
     private List<String> strings;
     private StringJoiner joiner;
+
     @Before
     public void setUp() throws Exception {
         strings = new ArrayList<String>();
         joiner = new StringJoiner();
     }
+
     @Test
     public void shouldJoinIntoAnEmptyStringWhenListIsEmpty(){
         assertThat(joiner.join(strings), is(""));
     }
+
     @Test
     public void shouldJoinIntoTheStringWhenListIsOneString(){
         String aString = "A String";
         strings.add(aString);
         assertThat(joiner.join(strings), is(aString));
+    }
+
+    @Test
+    public void shouldContainBothStringsWhenListIsTwoStrings(){
+        strings.add("A");
+        strings.add("B");
+        assertThat(joiner.join(strings), both(containsString("A")).and(containsString("B")));
     }
 }
