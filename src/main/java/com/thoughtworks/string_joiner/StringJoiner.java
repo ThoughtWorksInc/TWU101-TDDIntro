0c0ce6b1226593f27463b6ed1200778b0e680b20
Bill Schofield <bill@billschofield.net>
2014-04-17 17:32:31 -0700

    Finished string join functionality

--- src/main/java/com/thoughtworks/string_joiner/StringJoiner.java
+++ src/main/java/com/thoughtworks/string_joiner/StringJoiner.java
@@ -1,14 +1,22 @@
 package com.thoughtworks.string_joiner;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringJoiner {
+    private String delimiter;
+
     public StringJoiner(String delimiter) {
+        this.delimiter = delimiter;
     }
     public String join(List<String> strings) {
         String result = "";
-        for (String string : strings) {
-            result += string;
+        if (strings.size() > 0){
+            List<String> allExceptFirstString = new ArrayList<String>(strings);
+            result += allExceptFirstString.remove(0);
+            for (String string : allExceptFirstString) {
+                result += delimiter + string;
+            }
         }
         return result;
     }
