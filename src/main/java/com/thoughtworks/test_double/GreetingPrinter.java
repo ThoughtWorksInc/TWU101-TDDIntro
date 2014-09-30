a57d3f6960f2d2d0058a43bb9d8b5bfeff947897
Bill Schofield <bill@billschofield.net>
2014-09-30 09:54:11 -0500

    Improved Mocking & Stubbing Section

--- src/main/java/com/thoughtworks/test_double/GreetingPrinter.java
+++ src/main/java/com/thoughtworks/test_double/GreetingPrinter.java
@@ -3,20 +3,6 @@ package com.thoughtworks.test_double;
 import java.io.PrintStream;
 
 public class GreetingPrinter {
-
-    public static void main(String[] args) {
-        System.out.println("Greetings!");
-        // Can be refactored to...
-        PrintStream printStream = System.out;
-        printStream.println("Greetings!");
-
-        // Which can in turn be refactored to...
-        GreetingPrinter greetingPrinter = new GreetingPrinter(System.out);
-        greetingPrinter.printGreeting();
-    }
-
-    // Dependency Injection Example
-
     private PrintStream printStream;
 
     public GreetingPrinter(PrintStream printStream) {
